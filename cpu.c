e99263c2b1d1cb7c13a190fb5a3f4e864198d1e1
Sriram Ranganathan <sr7968@nyu.edu>
2026-05-28 18:59:54 -0400

    Progress on Preemptive Priority scheduling methods

--- cpu.c
+++ cpu.c
@@ -2,7 +2,41 @@
 #include <limits.h>
 #include <oslabs.h>
 
+// ============================ PRIORITY-BASED PREEMPTIVE SCHEDULING ALGORITHM ===================
+struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp) {
+    if (current_process.process_id == 0 && current_process.arrival_timestamp == 0 && current_process.total_bursttime == 0 && 
+        current_process.execution_starttime == 0 && current_process.execution_endtime == 0 && current_process.remaining_bursttime == 0 && 
+        current_process.process_priority == 0) {
+        // When there is no current process, new process is set to current timestamp
+        new_process.execution_starttime = timestamp;
+        new_process.execution_endtime = timestamp + new_process.total_bursttime;
+        new_process.remaining_bursttime = new_process.total_bursttime;
 
+        return new_process;
+    } else {
+        if (new_process.process_priority > current_process.process_priority) {
+            new_process.remaining_bursttime = new_process.total_bursttime;
+            new_process.execution_starttime = 0;
+            new_process.execution_endtime = 0;
+            ready_queue[*queue_cnt] = new_process;
+            (*queue_cnt)++;
+
+            return current_process;
+        } else {
+            current_process.execution_endtime = 0;
+            current_process.remaining_bursttime = current_process.total_bursttime - timestamp;
+            ready_queue[*queue_cnt] = current_process;
+            new_process.execution_starttime = timestamp;
+            new_process.execution_endtime = timestamp + new_process.total_bursttime;
+            new_process.remaining_bursttime = new_process.total_bursttime;
+            
+            return new_process;
+        }
+    }
+}
+struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp) {
+
+}
 
 // ============================ ROUND ROBIN SCHEDULING ALGORITHM =================================
 struct PCB handle_process_arrival_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp, int time_quantum) {
